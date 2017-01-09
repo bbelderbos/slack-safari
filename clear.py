bd5985ffd5c744836f90f3a2715c3f9f5a973795
bbelderbos <bobbelderbos@gmail.com>
2017-01-09 23:22:19 +0100

    add prints to clear script

--- clear.py
+++ clear.py
@@ -3,7 +3,9 @@ from safaribot import del_from_cache, clear_cache
 
 # if called with arg delete bookid, else clear whole cache
 if len(sys.argv) > 1:
+    print('deleting bookid {} from cache'.format(bookid))
     bookid = sys.argv[1]
     del_from_cache(bookid)
 else:
+    print('no arg, clearing whole cache')
     clear_cache()
